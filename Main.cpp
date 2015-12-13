6220a8e1f38f45bff36ac04218791a3a0155e7bb
Ruben Luna <rubenlunamate@gmail.com>
2015-12-13 13:36:20 -0430

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,9 +4,9 @@ Pilot (Visual Studio).
 #include <stdlib.h>
 #include "Primitivas.h"
 
-/* (x) lista principal.         (y) sub-lista. */
+// (x) lista principal.         (y) sub-lista. 
 
-void mostrar(principal *p) /*Muestra la lista principal completa.*/
+void mostrar(principal *p) //Muestra la lista principal completa.
 {
 	principal *aux = p;
 	while (aux)
@@ -18,7 +18,7 @@ void mostrar(principal *p) /*Muestra la lista principal completa.*/
 
 }
 
-void mostrarT(principal *p) /*Muestra un valor de la lista principal.*/
+void mostrarT(principal *p) //Muestra un valor de la lista principal.
 {
 	principal *aux = p;
 	if (aux->sig)
@@ -31,7 +31,7 @@ void mostrarT(principal *p) /*Muestra un valor de la lista principal.*/
 
 }
 
-void mostrar22(lista *p) /*Muestra toda una sub-lista, segun se le indique su principal.*/
+void mostrar22(lista *p) //Muestra toda una sub-lista, segun se le indique su principal.
 {
 	lista *aux = p;
 	while (aux)
@@ -42,7 +42,7 @@ void mostrar22(lista *p) /*Muestra toda una sub-lista, segun se le indique su pr
 	printf(" NULL");
 }
 
-void mostrar2(principal *p, int x) /*Busca el valor indicado (x) para mandar puntero a mostrar22 y mostrar su sub-lista.*/ /*Optimizar*/
+void mostrar2(principal *p, int x) //Busca el valor indicado (x) para mandar puntero a mostrar22 y mostrar su sub-lista.        Optimizar
 {
 	principal *aux = p;
 	if (!(vacio(p)))
@@ -60,7 +60,7 @@ void mostrar2(principal *p, int x) /*Busca el valor indicado (x) para mandar pun
 
 }
 
-void buscar(principal *p, int x) /*Buscar en el principal el valor (x) indicado para asi insertar en su sub-lista un valor (y).*/ /*Optimizar*/
+void buscar(principal *p, int x) //Buscar en el principal el valor (x) indicado para asi insertar en su sub-lista un valor (y).     Optimizar
 {
 	int z;
 	principal *aux = p;
@@ -80,7 +80,7 @@ void buscar(principal *p, int x) /*Buscar en el principal el valor (x) indicado 
 	}
 }
 
-void mostrarTodo(principal *p) /*Muestra cada caja de lista principal con su debida sub-lista.*/ /*Optimizar*/
+void mostrarTodo(principal *p) //Muestra cada caja de lista principal con su debida sub-lista.       Optimizar
 {
 	principal *aux = p; 
 	int cont = 1;
@@ -110,7 +110,7 @@ int buscarY(lista *p, int y)
 	return 0;
 }
 
-void secPrin(principal *p, int y) /*Toma un (x) y busca si tiene un (y) y asi mostrar todos los (x) que la contengan*/ /*Modificar*/
+void secPrin(principal *p, int y) //Toma un (x) y busca si tiene un (y) y asi mostrar todos los (x) que la contengan.        Modificar
 {
 	principal *aux = p;
 	if (!(vacio(p)))
@@ -124,13 +124,33 @@ void secPrin(principal *p, int y) /*Toma un (x) y busca si tiene un (y) y asi mo
 	}
 }
 
+int arch(FILE *archivo)
+{
+	char caracteres[100];
+
+	archivo = fopen("prueba.txt", "r");
+
+	if (archivo == NULL)
+		exit(1);
+
+	printf("\nEl contenido del archivo de prueba es \n\n");
+	while (feof(archivo) == 0)
+	{
+		fgets(caracteres, 100, archivo);
+		printf("%s", caracteres);
+	}
+
+	fclose(archivo);
+	return 0;
+}
+
 void main()
 {
-	int op = -1, n = 0;
+	int op = -1, n = 0; 
 	principal *p = NULL;
 	while (op != 0) {
 		system("cls");
-		printf("\n\n                              Menu principal \n\n\n.");
+		printf("\n\n                              Menu principal. \n\n\n");
 		printf("\n 1. Insertar en lista principal.");
 		printf("\n 2. Insertar a la sub-lista de un principal (x).");
 		printf("\n 3. Mostrar lista principal.");
@@ -138,17 +158,18 @@ void main()
 		printf("\n 5. Mostrar todo.");
 		printf("\n 6. Eliminar un elemento (x) de la lista principal.");
 		printf("\n 7. Buscar un (y) en las sub-listas y mostrar las (x) que las contienen.");
-		printf("\n 8. Guardar datos en un archivo."); /*No hecha*/
-		printf("\n 9. Leer datos desde el archivo."); /*No hecha*/
+		printf("\n 8. Guardar datos en un archivo."); //No hecha
+		printf("\n 9. Leer datos desde el archivo."); //No hecha
 		printf("0. Salir. \n\n");
 		printf("Indique opcion (1-5): ");
 		op = 0;
 		scanf("%i", &op);
 		system("cls");
 		printf("\n\n");
+		bool flag = true;
 		switch (op)
 		{
-			case 1: printf("Indique el tamanho de la lista principal: ");
+			case 1: printf("Indique el numero a ingresar: ");
 				scanf("%i", &n);
 				insertar(&p, n);
 				break;
@@ -177,12 +198,23 @@ void main()
 			case 6:
 				printf("Numero a buscar para eliminar de la lista principal (Elimina toda su sub-lista). :");
 				scanf("%i", &n);
-				eliminar(&p, n);
+				buscar(&p, n, flag);
 				break;
 			
 			case 7: printf("Numero de la sub-lista (y) a buscar su (x) en principal :");
 				scanf("%i", &n);
 				secPrin(p, n);
+				break;
+
+			case 9: FILE *archivo;
+				arch(archivo);
+				break;
+
+			case 10:
+				printf("Numero a buscar para eliminar de la lista principal \n(Guarda sublista en otra caja principal):");
+				scanf("%i", &n);
+				flag = false;
+				buscar(&p, n, flag);
 				break;
 		}
 		if (op)
